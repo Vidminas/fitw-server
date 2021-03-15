d299143e770863f8b3d80a0e20e931976073f255
Vidminas Mikucionis <5411598+Vidminas@users.noreply.github.com>
2021-03-15 10:25:10 +0000

    Fixup package.json for azure deployment

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,6 +1,7 @@
 import { cp } from "shelljs";
 
 cp("-u", `.env.${process.env.NODE_ENV || "prod"}`, "build/");
+cp("-u", "package.json", "build/");
 cp("-ur", "public/", "build/public/");
 cp("-ur", "src/layouts/", "build/layouts/");
 cp("-ur", "src/views/", "build/views/");
