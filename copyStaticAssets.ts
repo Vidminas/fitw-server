a50fc2362ab1f6cb3d370c5f836b2a237d08a544
Vidminas Mikucionis <5411598+Vidminas@users.noreply.github.com>
2021-03-18 11:36:31 +0000

    Fixup duplicate directories in static assets

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -2,6 +2,6 @@ import { cp } from "shelljs";
 
 cp("-u", `.env.${process.env.NODE_ENV || "prod"}`, "build/");
 cp("-u", "package.json", "build/");
-cp("-ur", "public/", "build/public/");
-cp("-ur", "src/layouts/", "build/layouts/");
-cp("-ur", "src/views/", "build/views/");
+cp("-ur", "public/", "build/");
+cp("-ur", "src/layouts/", "build/");
+cp("-ur", "src/views/", "build/");
